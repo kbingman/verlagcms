1db83eace3915eb7b357ed5a722845af62c69496
Keith Bingman <keith@keithbingman.com>
2011-05-27 12:47:20 +0200

    cleaning up

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -9,24 +9,35 @@ describe Page do
     teardown
   end
   
-  describe "validations" do
-    it "should create a valid page" do
+  describe 'validations' do
+    it 'should create a valid page' do
       Page.new(:title => 'Home', :site => @site).should be_valid
     end
    
-    it "should require a site" do
+    it 'should require a site' do
       Page.new(:title => 'Home', :site => nil).should_not be_valid
+    end 
+    
+    it 'should require a layout' do 
+      pending 'need to update the data'
+      Page.new(:title => '', :site => @site, :layout => nil).should_not be_valid
     end
     
-    it "should require a title" do
+    it 'should require a title' do
       Page.new(:title => '', :site => @site).should_not be_valid
     end  
   end
   
   context 'valid page' do
     before(:all) do  
+      @layout = Factory(:layout, :name => 'Layout', :site => @site, :content => '<h1>{{page.title}}</h1>')
       @root = Factory(:page, :title => 'root', :site => @site) 
-      @child = Factory(:page, :title => 'child', :parent_id => @root.id, :tag_list => 'tag1, tag2', :site => @site)
+      @child = Factory(:page, 
+        :title => 'Child', 
+        :parent_id => @root.id, 
+        :tag_list => 'tag1, tag2',
+        :layout => @layout, 
+        :site => @site)
     end
      
     describe 'attributes' do 
@@ -72,7 +83,7 @@ describe Page do
       end 
     end 
     
-    describe 'finders' do
+    describe '#find_by_path' do
       it 'should find the root page' do
         Page.find_by_path('/', @site).should == @root    
       end
@@ -96,7 +107,22 @@ describe Page do
       it 'should find pages by all tags' do
         Page.search_all('tag1 tag5').all.should_not == [@child]
       end
+    end  
+    
+    describe '#render' do  
+      it 'should have a layout' do
+        @child.layout.should_not be_nil
+      end
+      
+      it 'should respond to render' do
+        @child.should respond_to(:render)
+      end   
+      
+      it 'should render the page' do
+        @child.render.should == '<h1>Child</h1>'
+      end
     end
+       
   end
   
 end

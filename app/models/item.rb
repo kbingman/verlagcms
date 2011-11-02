0a6721c0f1ff09d78081a9b03c796f1b970b81ba
Keith Bingman <keith@keithbingman.com>
2011-11-02 21:45:27 +0100

    cleanup

--- app/models/item.rb
+++
@@ -1,8 +0,0 @@
-class Item
-  include MongoMapper::Document 
-  
-  key :site_id, ObjectId 
-  belongs_to :site, :foreign_key => :site_id 
-  scope :by_site,  lambda { |site| where(:site_id => site.id) }
-  
-end

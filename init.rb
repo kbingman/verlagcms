9adc6f51b93ba5f7a4b7e137d97d219cad2cfe4e
Keith Bingman <keith@keithbingman.com>
2012-01-23 12:09:39 +0100

    require

--- init.rb
+++ init.rb
@@ -168,8 +168,9 @@ end
 # These need to be required first or Page blows up...
 # require root_path('app/models/templates/template.rb')
 
-require root_path('app/models/assets/asset.rb')
 require root_path('app/models/parts/part.rb')
+require root_path('app/models/assets/asset.rb')
+
 # Load all models.
 Dir[root_path('app/models/**/*.rb')].each do |file|
   require file
